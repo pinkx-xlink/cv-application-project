e426375bae64a8947fbd438e049ae4039912db3c
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-11-21 18:39:55 -0500

    Create "general-info" container div

--- cv-app-project/src/App.js
+++ cv-app-project/src/App.js
@@ -29,35 +29,37 @@ function App() {
       <header className="App-header">
         <h1>CV Creator Project</h1>
         <p>Enter your info and we'll take care of the rest!</p>
-
-        <label>
-          First name: {' '}
-          <input 
-            value={firstName}
-            onChange={handleFirstNameChange}
-          />
-        </label>
-        <label>
-          Last name: {' '}
-          <input 
-            value={lastName}
-            onChange={handleLastNameChange}
-          />
-        </label>
-        <label>
-          Email: {' '}
-          <input 
-            value={email}
-            onChange={handleEmailChange}
-          />
-        </label>
-        <label>
-          Phone number: {' '}
-          <input
-            value={phoneNumber}
-            onChange={handlePhoneNumberChange}
-          />
-        </label>
+        <div class="general-info"> 
+          <h3>General Info</h3>
+          <label>
+            First name: {' '}
+            <input 
+              value={firstName}
+              onChange={handleFirstNameChange}
+            />
+          </label>
+          <label>
+            Last name: {' '}
+            <input 
+              value={lastName}
+              onChange={handleLastNameChange}
+            />
+          </label>
+          <label>
+            Email: {' '}
+            <input 
+              value={email}
+              onChange={handleEmailChange}
+            />
+          </label>
+          <label>
+            Phone number: {' '}
+            <input
+              value={phoneNumber}
+              onChange={handlePhoneNumberChange}
+            />
+          </label>
+        </div>
 
         <div id="cv-form">
           <h2>{fullName}</h2>
