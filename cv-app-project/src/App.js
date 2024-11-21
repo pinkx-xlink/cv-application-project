1dde970d93293322bb040273e491c7928a1e7e6a
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-11-21 18:10:57 -0500

    Display fullName

--- cv-app-project/src/App.js
+++ cv-app-project/src/App.js
@@ -4,10 +4,40 @@ import { useState } from 'react';
 function App() {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
+
+  const fullName = firstName + ' ' + lastName;
+  function handleFirstNameChange(e) {
+    setFirstName(e.target.value);
+  }
+
+  function handleLastNameChange(e) {
+    setLastName(e.target.value);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>CV Creator Project</h1>
+        <p>Enter your info and we'll take care of the rest!</p>
+
+        <label>
+          First name: {' '}
+          <input 
+            value={firstName}
+            onChange={handleFirstNameChange}
+          />
+        </label>
+        <label>
+          Last name: {' '}
+          <input 
+            value={lastName}
+            onChange={handleLastNameChange}
+          />
+        </label>
+
+        <div>
+          <h2>{fullName}</h2>
+        </div>
       </header>
     </div>
   );
